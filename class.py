3c39dbfc6c5ecf579e43f84c6b4f01c9113fd540
HolyCash <37403502+HolyCash@users.noreply.github.com>
2021-01-16 14:55:04 +0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,9 @@
+class Apple:
+    def __init__(self, c, s, p):
+        self.color = c
+        self.size = s 
+        self.price = p 
+app1 = Apple("green", "big", 75)
+print (app1.color)
+print (app1.size)
+print (app1.price)
